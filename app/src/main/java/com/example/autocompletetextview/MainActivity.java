c22470b9c97f85d0a848f81f85cb8bb6cb53ebf2
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-01-28 09:03:24 +0600

    Second Commit AutoCompleteTextView

--- app/src/main/java/com/example/autocompletetextview/MainActivity.java
+++ app/src/main/java/com/example/autocompletetextview/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.autocompletetextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView autoCompleteTextView;
+    private String[] countryName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView_Id);
+        countryName = getResources().getStringArray(R.array.county_names);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,countryName);
+
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(adapter);
+
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String value = autoCompleteTextView.getText().toString();
+
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
     }
 }
